b78e3002f3b5e481e5aa856dda9c75d829ff9997
Azat Gilyazov <azatpscl@gmail.com>
2020-03-18 15:18:54 +0300

    Added new Homework
    Added new IOStream homework

--- src/Core.java
+++ src/Core.java
@@ -1,10 +1,13 @@
-import IT.HW7.StreamHW;
+import IT.HW8.IOStreamsHW;
+import java.io.IOException;
+
 
 public class Core {
-    public static void main(String[] args) throws CloneNotSupportedException {
-        StreamHW test = new StreamHW();
+    public static void main(String[] args) throws IOException {
+        long startTime = System.currentTimeMillis();
+        IOStreamsHW test = new IOStreamsHW();
         test.run();
-
-
+        long finishTime = System.currentTimeMillis();
+        System.out.println("Runtime = " + (finishTime - startTime) + " ms");
     }
 }
