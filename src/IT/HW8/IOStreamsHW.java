bb6601e40cfd43bc614ae63e7bad606930045cea
Azat Gilyazov <azatpscl@gmail.com>
2020-03-18 22:32:06 +0300

    Fixed bugs in last hw
    Соnverted byte[] array to int[] arrray

--- src/IT/HW8/IOStreamsHW.java
+++ src/IT/HW8/IOStreamsHW.java
@@ -6,12 +6,12 @@ public class IOStreamsHW {
 
     public static void run() {
         try(OutputStream out = new FileOutputStream("src\\IT\\HW8\\test");){
-            int v = 1124;
+            int v = -994569;
             char c = 'ф';
-            out.write(v >> 24 & 0xFF);
-            out.write(v >> 16 & 0xFF);
-            out.write(v >>  8 & 0xFF);
-            out.write(v >>  0 & 0xFF);
+            out.write(v >>> 24 & 0xFF);
+            out.write(v >>> 16 & 0xFF);
+            out.write(v >>>  8 & 0xFF);
+            out.write(v >>>  0 & 0xFF);
             out.write(c >> 8 & 0xFF);
             out.write(c >> 0 & 0xFF);
             out.flush();
@@ -21,16 +21,16 @@ public class IOStreamsHW {
         }
 
         try(InputStream in = new FileInputStream("src\\IT\\HW8\\test")){
-            byte[] data = new byte[256];
+            int[] data = new int[2563443];
             int i = 0;
             int b;
             while((b = in.read()) != -1){
-                data[i++] = (byte) b;
+                data[i++] =  b;
             }
 
             System.out.println("Integer from file = " + ((data[0] << 24) + (data[1] << 16) + (data[2] << 8) + (data[3])));
             System.out.println("Char from file = " + (char)((data[4]<<8) + data[5]));
-;        }
+        }
         catch (IOException e){
             System.out.println("File not found!");
         }
